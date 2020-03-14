8b6d5a44bf1422f2e781d3d022ad50cc60bb139b
neilaldrei <neilaldreiiv@gmail.com>
2020-03-14 15:26:44 +0800

    Lesson #19: Cards:

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,37 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: "Dio", text: "You thought it was Lavren, but it was me! DIO")
   ];
 
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.grey[600]
+              )
+            ),
+
+            SizedBox(height: 6.0),
+
+            Text(
+              quote.author,
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey[800]
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,7 +60,7 @@ class _QuoteListState extends State<QuoteList> {
       ),
 
       body: Column(
-        children: quotes.map((quote) => Text('${ quote.text } - ${ quote.author }')).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       )
     );
   }
